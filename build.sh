29cf963eb81ebf5690f9f36c4ea3a7de982e58c0
samosica <samosic4@gmail.com>
2024-07-19 06:01:28 +0900

    Fix Pure version

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 CDIR="$(cd "$(dirname "$0")" && pwd)"
 build_dir=$CDIR/build
 
+readonly PURE_VERSION=v1.23.0
+
 while getopts A:K:q option
 do
   case "${option}"
@@ -26,7 +28,10 @@ cd $build_dir
 [ $QUIET ] && arg_q='-q' || arg_q=''
 
 if [ -x "$(command -v git)" ]; then
-    git clone $arg_q --depth 1 https://github.com/sindresorhus/pure.git "$build_dir/pure"
+    echo "[info] install Pure ${PURE_VERSION}"
+    # do not show detached head warning
+    # shellcheck disable=SC2086
+    git clone $arg_q --depth 1 --branch "$PURE_VERSION" https://github.com/sindresorhus/pure.git "$build_dir/pure" 2>/dev/null
 else
     echo Install git
     exit 1
