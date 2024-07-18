53acb6e466031d2c1b81a8d2bcaa3f48bde63440
samosica <samosic4@gmail.com>
2024-07-19 06:12:29 +0900

    Clean up

--- build.sh
+++ build.sh
@@ -1,38 +1,34 @@
 #!/usr/bin/env bash
+set -eu
 
 CDIR="$(cd "$(dirname "$0")" && pwd)"
-build_dir=$CDIR/build
-
+readonly CDIR
+readonly build_dir=$CDIR/build
 readonly PURE_VERSION=v1.23.0
 
-while getopts A:K:q option
-do
-  case "${option}"
-  in
-    q) QUIET=1;;
-    A) ARCH=${OPTARG};;
-    K) KERNEL=${OPTARG};;
-  esac
+QUIET=""
+
+while getopts A:K:q option; do
+    case "${option}" in
+        q) QUIET=1;;
+        *) ;;
+    esac
 done
 
-rm -rf $build_dir
-mkdir -p $build_dir
+rm -rf "$build_dir"
+mkdir -p "$build_dir"
+cp "$CDIR/pluginrc.zsh" "$build_dir/"
 
-for f in pluginrc.zsh
-do
-    cp $CDIR/$f $build_dir/
-done
+cd "$build_dir" || exit 1
 
-cd $build_dir
+[ "$QUIET" ] && arg_q='-q' || arg_q=''
 
-[ $QUIET ] && arg_q='-q' || arg_q=''
-
-if [ -x "$(command -v git)" ]; then
-    echo "[info] install Pure ${PURE_VERSION}"
-    # do not show detached head warning
-    # shellcheck disable=SC2086
-    git clone $arg_q --depth 1 --branch "$PURE_VERSION" https://github.com/sindresorhus/pure.git "$build_dir/pure" 2>/dev/null
-else
-    echo Install git
+if ! command -v git >/dev/null 2>&1; then
+    echo "[error] you need to install git"
     exit 1
 fi
+
+echo "[info] install Pure ${PURE_VERSION}"
+# do not show detached head warning
+# shellcheck disable=SC2086
+git clone $arg_q --depth 1 --branch "$PURE_VERSION" https://github.com/sindresorhus/pure.git "$build_dir/pure" 2>/dev/null
