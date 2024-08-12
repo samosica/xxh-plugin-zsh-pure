743348276139401f7052c2906730c364bf3027c7
samosica <samosic4@gmail.com>
2024-08-12 15:28:54 +0900

    Fix

--- scripts/close-outdated-prs.sh
+++ scripts/close-outdated-prs.sh
@@ -23,6 +23,7 @@ EOF
 
 read_args(){
     set +u
+    local opt
     while getopts hp: opt; do
         case "$opt" in
             h) usage; exit 0 ;;
@@ -41,17 +42,19 @@ read_args(){
 }
 
 retrieve_prs(){
-    readonly FILTER=".[] | .headRefName | select(test(\"^$PREFIX\"))"
+    local FILTER=".[] | .headRefName | select(test(\"^$PREFIX\"))"
     gh pr list --json headRefName --jq "$FILTER"
 }
 
 compare_versions(){
+    local major1 minor1 patch1
+    local major2 minor2 patch2
     IFS='.' read -r major1 minor1 patch1 < <(echo "${1#v}")
     IFS='.' read -r major2 minor2 patch2 < <(echo "${2#v}")
 
-    diff_major=$(("$major1" - "$major2"))
-    diff_minor=$(("$minor1" - "$minor2"))
-    diff_patch=$(("$patch1" - "$patch2"))
+    local diff_major=$(("$major1" - "$major2"))
+    local diff_minor=$(("$minor1" - "$minor2"))
+    local diff_patch=$(("$patch1" - "$patch2"))
 
     if ! [ "$diff_major" -eq 0 ]; then
         echo "$diff_major"
